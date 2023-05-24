6c000dd8164f25f21ecb2135dd2c50e1a8d308ab
rychardbarros <rychard.barros.dev@gmail.com>
2023-05-24 15:05:13 -0300

    'ListaSimplesmenteEncadeada'

--- 02 Listas Simplesmente Encadeadas/main.py
+++ 02 Listas Simplesmente Encadeadas/main.py
@@ -14,6 +14,17 @@ class ListPatient():
     def add_patient(self, id, name ,health_status):
         new_patient = Patient(id, name, health_status)
         if self.head is None:
+            # Se a lista estiver vazia, o novo paciente é adicionado como cabeça da lista
             self.head = new_patient
         else:
+            # Caso contrário, o paciente é adicionado ao final da lista
             present_patient = self.head
+            while present_patient.next_patient is not None:
+                present_patient = present_patient.next_patient
+            present_patient = new_patient
+
+    # Método para remover um paciente da lista, recebe como parâmetro o id do paciente a ser removido
+    def remove_patient(self, id):
+        if self.head is None:
+
+            return
