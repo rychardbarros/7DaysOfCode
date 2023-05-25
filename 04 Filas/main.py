669525b4fca6b4d1377c07d0517f79317a7473da
rychardbarros <rychard.barros.dev@gmail.com>
2023-05-25 15:46:46 -0300

    'Filas'

---
+++ 04 Filas/main.py
@@ -0,0 +1,28 @@
+class Order():
+    def __init__(self, number, name, food):
+        self.number = number
+        self.name = name
+        self.food = food
+
+class ListOrder():
+    def __init__(self):
+        self.list_order = []
+
+    def add_order(self, number, name, food):
+        new_order = Order(number, name, food)
+        self.list_order.append(new_order)
+
+    def remove_order(self):
+        if len(self.list_order) < 1:
+            print('Não há pedidos no momento!')
+            return 
+        else:
+            return self.list_order.pop(0)
+
+    def list_order(self):
+        if len(self.list_order) < 1:
+            print('Não há pedidos no momento!')
+            return
+        else:
+            for order in self.list_order:
+                print(f'Numero:{order.number}, Nome: {order.name}, Prato:{order.food}')
