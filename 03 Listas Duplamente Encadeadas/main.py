b69302aaeff8023cb0f8d80d1f9e5fe5d65a89c7
rychardbarros <rychard.barros.dev@gmail.com>
2023-05-25 13:59:59 -0300

    'ListaDuplamenteEncadeada'

--- 03 Listas Duplamente Encadeadas/main.py
+++ 03 Listas Duplamente Encadeadas/main.py
@@ -6,3 +6,20 @@ class Product():
         self.amount = amount
         self.next_product = None
         self.previous_product = None
+
+class ListProduct():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_product(self, bar_code, name, value, amount):
+        new_product = Product(bar_code, name, value, amount):
+        if self.head is None:
+            self.head = new_product
+            self.tail = new_product
+        else:
+            self.tail.next_product = new_product
+            new_product.previous_product = self.tail
+            self.tail = new_product
+
+    
