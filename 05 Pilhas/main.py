90abf7f4d750c34d49eec841ddaaa18909879ac4
rychardbarros <rychard.barros.dev@gmail.com>
2023-05-26 14:24:48 -0300

    'Pilhas'

---
+++ 05 Pilhas/main.py
@@ -0,0 +1,8 @@
+class Book():
+    def __init__(self, name, number_page):
+        self.name = name
+        self.name = number_page
+
+class ListBook():
+    def add_book(self, name, number_page):
+        
