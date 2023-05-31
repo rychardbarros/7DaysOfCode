d00c7cadf726287e19a3acb57a9a744881b1f84a
rychardbarros <rychard.barros.dev@gmail.com>
2023-05-31 14:39:47 -0300

    'Hashmap'

--- 06 Hashmap/main.py
+++ 06 Hashmap/main.py
@@ -7,7 +7,7 @@ class Game():
     
     def update_score(self, user, score):
         if user in self.score:
-            self.score[user] += score
+            self.score[user] += float(score)
 
     def remove_user(self, user):
         del self.score[user]
@@ -18,7 +18,7 @@ class Game():
             return
         ranking = sorted(self.score.items(), key=lambda x: x[1], reverse=True)
         for user, score in ranking:
-            print(f'{user}, pontos:{score}')
+            print(f'{user}, pontos: {score}')
 
     def get_winner(self):
         max_score = max(self.score.values())
@@ -26,3 +26,22 @@ class Game():
             if score == max_score:
                 print(f'O vencedor foi {user} com {score} pontos')
         return user
+
+game = Game()
+
+game.add_player('MrRychk')
+game.add_player('italoMtitus')
+game.add_player('r3nanp')
+
+game.update_score('MrRychk', '80')
+game.update_score('italoMtitus', '60')
+game.update_score('r3nanp', '40')
+
+game.list_score()
+
+game.remove_user('r3nanp')
+
+game.list_score()
+
+game.get_winner()
+
